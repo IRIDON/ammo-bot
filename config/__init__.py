3e686f134e86e9aa0c13076778fc27c630be25ab
IRIDON <iridon2@gmail.com>
2021-04-19 19:51:49 +0300

    refactor shops settings to caliber constanst

--- config/__init__.py
+++ config/__init__.py
@@ -3,6 +3,30 @@ from key import default_api_key as apiKey
 
 path = os.path.abspath(os.path.split(sys.argv[0])[0])
 
+CALIBER_223REM = "223_Rem"
+CALIBER_308WIN = "308_Win"
+CALIBER_338LM = "338_Lapua_Mag"
+CALIBER_762X39 = "7.62x39"
+CALIBER_22LR = "22_LR"
+CALIBER_17HMR = "17_HMR"
+CALIBER_3006 = "30-06"
+CALIBER_300WM = "300_Win_Mag"
+CALIBER_65C = "6.5_Creedmoor"
+CALIBER_300ACC = "300_AAC"
+CALIBER_921 = "9x21"
+CALIBER_22WNR = "22_WMR"
+CALIBER_300WIN = "300_Win"
+CALIBER_545X39 = "5.45x39"
+CALIBER_762X54R = "7.62x54_R"
+CALIBER_8X57JS = "8x57_JS"
+CALIBER_222REM = "222_Rem"
+CALIBER_9RA = "9RA"
+CALIBER_7REMMAG = "7mm_Rem_Mag"
+CALIBER_1270 = "12/70"
+CALIBER_1276 = "12/76"
+CALIBER_1670 = "16/70"
+CALIBER_2070 = "20/70"
+
 default_settings = {
     "API_TOKEN": apiKey["BOT_API_TOKEN"],
     "DEV": apiKey["DEV"],
@@ -16,30 +40,29 @@ default_settings = {
     "DISCONT": [0, 3, 5, 10, 15, 20, 25],
     "CURRENCY": "UAH",
     "CALIBERS": [
-        "223_Rem",
-        "308_Win",
-        "338_Lapua_Mag",
-        "7.62x39",
-        "22_LR",
-        "17_HMR",
-        "30-06",
-        "300_Win_Mag",
-        "6.5_Creedmoor",
-        "300_AAC",
-        "9x21",
-        "22_WMR",
-        "300_Win",
-        "5.45x39",
-        "7.62x54_R",
-        "8x57_JS",
-        "222_Rem",
-        "9RA",
-        "7mm_Rem_Mag",
-        "338_Win_Mag",
-        "12/70",
-        "12/76",
-        "16/70",
-        "20/70"
+        CALIBER_223REM,
+        CALIBER_308WIN,
+        CALIBER_338LM,
+        CALIBER_762X39,
+        CALIBER_22LR,
+        CALIBER_17HMR,
+        CALIBER_3006,
+        CALIBER_300WM,
+        CALIBER_65C,
+        CALIBER_300ACC,
+        CALIBER_921,
+        CALIBER_22WNR,
+        CALIBER_300WIN,
+        CALIBER_545X39,
+        CALIBER_762X54R,
+        CALIBER_8X57JS,
+        CALIBER_222REM,
+        CALIBER_9RA,
+        CALIBER_7REMMAG,
+        CALIBER_1270,
+        CALIBER_1276,
+        CALIBER_1670,
+        CALIBER_2070,
     ]
 }
 
--- config/shops.py
+++ config/shops.py
@@ -1,5 +1,7 @@
 import os, sys
 
+from config import CALIBER_223REM, CALIBER_308WIN, CALIBER_338LM, CALIBER_762X39, CALIBER_22LR, CALIBER_17HMR, CALIBER_3006, CALIBER_300WM, CALIBER_65C, CALIBER_300ACC, CALIBER_921, CALIBER_22WNR, CALIBER_300WIN, CALIBER_545X39, CALIBER_762X54R, CALIBER_8X57JS, CALIBER_222REM, CALIBER_9RA, CALIBER_7REMMAG, CALIBER_1270, CALIBER_1276, CALIBER_1670, CALIBER_2070
+
 path = os.path.abspath(os.path.split(sys.argv[0])[0])
 data_path = path + "/data/"
 
@@ -16,33 +18,33 @@ shops = {
             "flober": "patrony-flobera",
         },
         "category": {
-            "223_Rem": ["223-rem-sw8", "rifle", 0],
-            "308_Win": ["308-win-dgs", "rifle", 1],
-            "338_Lapua_Mag": ["338-lapua-mag-1s0", "rifle", 2],
-            "7.62x39": ["762h39", "rifle", 3],
-            "22_LR": ["22-lr_snw", "rifle", 4],
-            "17_HMR": ["17-hmr_e94", "rifle", 5],
-            "30-06": ["30-06-s8e", "rifle", 6],
-            "300_Win_Mag": ["300-win-mag-0e0", "rifle", 7],
-            "6.5_Creedmoor": ["65-creedmoor", "rifle", 8],
-            "300_AAC": ["300-whisperblackout", "rifle", 9],
-            "22_WMR": ["22-wmr_tgr", "rifle", 11],
-            "7.62x54_R": ["762h54-r", "rifle", 12],
-            "222_Rem": ["222-rem-99s", "rifle", 13],
+            CALIBER_223REM: ["223-rem-sw8", "rifle", 0],
+            CALIBER_308WIN: ["308-win-dgs", "rifle", 1],
+            CALIBER_921: ["9mm-9x21", "rifle", 51],
+            CALIBER_762X39: ["762h39", "rifle", 3],
+            CALIBER_22LR: ["22-lr_snw", "rifle", 4],
+            CALIBER_17HMR: ["17-hmr_e94", "rifle", 5],
+            CALIBER_338LM: ["338-lapua-mag-1s0", "rifle", 2],
+            CALIBER_3006: ["30-06-s8e", "rifle", 6],
+            CALIBER_65C: ["65-creedmoor", "rifle", 8],
+            CALIBER_300ACC: ["300-whisperblackout", "rifle", 9],
+            CALIBER_762X54R: ["762h54-r", "rifle", 12],
+            CALIBER_8X57JS: ["8h57-js", "rifle", 60],
+            CALIBER_22WNR: ["22-wmr_tgr", "rifle", 11],
+            CALIBER_222REM: ["222-rem-99s", "rifle", 13],
+            CALIBER_545X39: ["545h39", "rifle", 52],
+            CALIBER_300WM: ["300-win-mag-0e0", "rifle", 7],
+            CALIBER_7REMMAG: ["7mm-rem-mag", "rifle", 55],
             "243_Win": ["243-win-1i9", "rifle", 50],
-            "9x21": ["9mm-9x21", "rifle", 51],
-            "5.45x39": ["545h39", "rifle", 52],
             "50_BMG": ["50-bmg", "rifle", 54],
-            "7mm_Rem_Mag": ["7mm-rem-mag", "rifle", 55],
-            "8x57_JS": ["8h57-js", "rifle", 60],
             "9.3x62": ["93h62", "rifle", 61],
 
-            "9RA": ["9-mm_m16/9-mm-ra", "traumatic", 70],
+            CALIBER_9RA: ["9-mm_m16/9-mm-ra", "traumatic", 70],
 
-            "12/70": ["1270", "bore", 80],
-            "12/76": ["1276", "bore", 81],
-            "16/70": ["1670", "bore", 82],
-            "20/70": ["2070", "bore", 83],
+            CALIBER_1270: ["1270", "bore", 80],
+            CALIBER_1276: ["1276", "bore", 81],
+            CALIBER_1670: ["1670", "bore", 82],
+            CALIBER_2070: ["2070", "bore", 83],
         }
     },
     "stvol": {
@@ -56,32 +58,32 @@ shops = {
             "traumatic": "patrony_travmaticheskogo_deystviya"
         },
         "category": {
-            "223_Rem": ["91bb3ef4-f38b-11e2-bd5e-1cc1de282c58", "rifle", 0],
-            "308_Win": ["99957513-f397-11e2-bd5e-1cc1de282c58", "rifle", 1],
-            "338_Lapua_Mag": ["ca7b6293-be7d-11e4-bddc-1cc1de282c58", "rifle", 2],
-            "22_LR": ["ada64ea2-f397-11e2-bd5e-1cc1de282c58", "rifle", 3],
-            "17_HMR": ["b522825f-f397-11e2-bd5e-1cc1de282c58", "rifle", 4],
-            "30-06": ["c20cd9bc-f397-11e2-bd5e-1cc1de282c58", "rifle", 5],
-            "300_Win_Mag": ["cf2df0be-f397-11e2-bd5e-1cc1de282c58", "rifle", 6],
+            CALIBER_223REM: ["91bb3ef4-f38b-11e2-bd5e-1cc1de282c58", "rifle", 0],
+            CALIBER_308WIN: ["99957513-f397-11e2-bd5e-1cc1de282c58", "rifle", 1],
+            CALIBER_338LM: ["ca7b6293-be7d-11e4-bddc-1cc1de282c58", "rifle", 2],
+            CALIBER_22LR: ["ada64ea2-f397-11e2-bd5e-1cc1de282c58", "rifle", 3],
+            CALIBER_17HMR: ["b522825f-f397-11e2-bd5e-1cc1de282c58", "rifle", 4],
+            CALIBER_3006: ["c20cd9bc-f397-11e2-bd5e-1cc1de282c58", "rifle", 5],
+            CALIBER_300WM: ["cf2df0be-f397-11e2-bd5e-1cc1de282c58", "rifle", 6],
+            CALIBER_65C: ["7c63096b-6b9c-11e7-ab40-00155dfa860b", "rifle", 8],
+            CALIBER_762X39: ["6ed3512f-f38b-11e2-bd5e-1cc1de282c58", "rifle", 9],
+            CALIBER_222REM: ["619213a1-f397-11e2-bd5e-1cc1de282c58", "rifle", 10],
+            CALIBER_762X54R: ["kalibr-is-9a7138f2-b03a-11e4-bc08-1cc1de282c58", "rifle", 50],
+            CALIBER_921: ["d6ac461a-d936-11e4-8e15-1cc1de282c58", "rifle", 51],
+            CALIBER_545X39: ["4f22644d-be84-11e4-bddc-1cc1de282c58", "rifle", 52],
+            CALIBER_7REMMAG: ["05d3a7d3-d94a-11e4-8e15-1cc1de282c58", "rifle", 55],
+            CALIBER_300ACC: ["b79860b6-1837-11e6-9b00-1cc1de282c58", "rifle", 53],
+            CALIBER_8X57JS: ["kalibr-is-bd7a32ad-f398-11e2-bd5e-1cc1de282c58", "rifle", 71],
             "338_Win_Mag": ["f37388bc-bb65-11e4-8f94-1cc1de282c58", "rifle", 7],
-            "6.5_Creedmoor": ["7c63096b-6b9c-11e7-ab40-00155dfa860b", "rifle", 8],
-            "7.62x39": ["6ed3512f-f38b-11e2-bd5e-1cc1de282c58", "rifle", 9],
-            "222_Rem": ["619213a1-f397-11e2-bd5e-1cc1de282c58", "rifle", 10],
-            "7.62x54R": ["kalibr-is-9a7138f2-b03a-11e4-bc08-1cc1de282c58", "rifle", 50],
-            "9x21": ["d6ac461a-d936-11e4-8e15-1cc1de282c58", "rifle", 51],
-            "5.45x39": ["4f22644d-be84-11e4-bddc-1cc1de282c58", "rifle", 52],
-            "300_AAC": ["b79860b6-1837-11e6-9b00-1cc1de282c58", "rifle", 53],
             "6.5x55": ["68682503-f398-11e2-bd5e-1cc1de282c58", "rifle", 54],
-            "7mm_Rem_Mag": ["05d3a7d3-d94a-11e4-8e15-1cc1de282c58", "rifle", 55],
             "9.3x62": ["247b80d0-f398-11e2-bd5e-1cc1de282c58", "rifle", 70],
-            "8x57_JS": ["kalibr-is-bd7a32ad-f398-11e2-bd5e-1cc1de282c58", "rifle", 71],
 
-            "9RA": ["f5f0d571-640b-11e4-9e41-1cc1de282c58", "traumatic", 80],
+            CALIBER_9RA: ["f5f0d571-640b-11e4-9e41-1cc1de282c58", "traumatic", 80],
 
-            "12/70": ["a45bde61-d219-11e4-8e15-1cc1de282c58", "bore", 90],
-            "12/76": ["5998ccc0-d90e-11e4-8e15-1cc1de282c58", "bore", 91],
-            "16/70": ["114c2f3e-d210-11e4-8e15-1cc1de282c58", "bore", 92],
-            "20/70": ["d44ea6b4-d204-11e4-8e15-1cc1de282c58", "bore", 93]
+            CALIBER_1270: ["a45bde61-d219-11e4-8e15-1cc1de282c58", "bore", 90],
+            CALIBER_1276: ["5998ccc0-d90e-11e4-8e15-1cc1de282c58", "bore", 91],
+            CALIBER_1670: ["114c2f3e-d210-11e4-8e15-1cc1de282c58", "bore", 92],
+            CALIBER_2070: ["d44ea6b4-d204-11e4-8e15-1cc1de282c58", "bore", 93]
         }
     },
     "safari": {
@@ -96,14 +98,18 @@ shops = {
             "flober": "patrony-flobera/c178839"
         },
         "category": {
-            "223_Rem": ["5456", "rifle", 0],
-            "308_Win": ["5475", "rifle", 1],
-            "22_LR": ["5451", "rifle", 2],
-            "17_HMR": ["5448", "rifle", 3],
-            "30-06": ["5466", "rifle", 4],
-            "300_Win_Mag": ["5473", "rifle", 5],
-            "6.5_Creedmoor": ["6535", "rifle", 6],
-            "7.62x54_R": ["5534", "rifle", 7],
+            CALIBER_223REM: ["5456", "rifle", 0],
+            CALIBER_308WIN: ["5475", "rifle", 1],
+            CALIBER_22LR: ["5451", "rifle", 2],
+            CALIBER_17HMR: ["5448", "rifle", 3],
+            CALIBER_3006: ["5466", "rifle", 4],
+            CALIBER_300WM: ["5473", "rifle", 5],
+            CALIBER_65C: ["6535", "rifle", 6],
+            CALIBER_300ACC: ["5472", "rifle", 53],
+            CALIBER_762X54R: ["5534", "rifle", 7],
+            CALIBER_8X57JS: ["5544", "rifle", 61],
+            CALIBER_545X39: ["5525", "rifle", 52],
+            CALIBER_7REMMAG: ["5536", "rifle", 54],
             "270_Win": ["5462", "rifle", 8],
             "280_Rem": ["5464", "rifle", 9],
             "30_R_Blaser": ["5465", "rifle", 10],
@@ -111,22 +117,17 @@ shops = {
             "375_H_&_H_Mag": ["5485", "rifle", 12],
             "470_Nitro-Express": ["5506", "rifle", 50],
             "9mm": ["5547", "rifle", 51],
-            "5.45x39": ["5525", "rifle", 52],
-            "300_AAC": ["5472", "rifle", 53],
-            "7mm_Rem_Mag": ["5536", "rifle", 54],
             "7x64": ["5541", "rifle", 55],
             "7x65_R": ["5542", "rifle", 56],
             "8x57_JRS": ["5543", "rifle", 60],
-            "8x57_JS": ["5544", "rifle", 61],
             "9.3x74_R": ["5552", "rifle", 62],
-            "8x57_JS": ["5544", "rifle", 63],
 
-            "9RA": ["5384", "traumatic", 70],
+            CALIBER_9RA: ["5384", "traumatic", 70],
 
-            "12/70": ["5371", "bore", 80],
-            "12/76": ["5372", "bore", 81],
-            "16/70": ["5375", "bore", 82],
-            "20/70": ["5377", "bore", 83],
+            CALIBER_1270: ["5371", "bore", 80],
+            CALIBER_1276: ["5372", "bore", 81],
+            CALIBER_1670: ["5375", "bore", 82],
+            CALIBER_2070: ["5377", "bore", 83],
         }
     },
     "kulya": {
@@ -142,27 +143,27 @@ shops = {
             "bore_birdshot": "gladkostvolnye/drob/kalibr-gladkostvolnyj-"
         },
         "category": {
-            "223_Rem": ["-223-rem", "rifle", 0],
-            "308_Win": ["-308-win", "rifle", 1],
-            "7.62x39": ["7-62x39", "rifle", 2],
-            "22_LR": ["-22lr", "rifle", 3],
-            "17_HMR": ["-17-hmr", "rifle", 3],
-            "30-06": ["-30-06", "rifle", 4],
-            "7.62x54_R": ["7-62x54-r", "rifle", 5],
-            "9x21": ["9h21", "rifle", 6],
-            "22_WMR": ["-22-wmr", "rifle", 50],
+            CALIBER_223REM: ["-223-rem", "rifle", 0],
+            CALIBER_308WIN: ["-308-win", "rifle", 1],
+            CALIBER_762X39: ["7-62x39", "rifle", 2],
+            CALIBER_22LR: ["-22lr", "rifle", 3],
+            CALIBER_17HMR: ["-17-hmr", "rifle", 3],
+            CALIBER_3006: ["-30-06", "rifle", 4],
+            CALIBER_762X54R: ["7-62x54-r", "rifle", 5],
+            CALIBER_921: ["9h21", "rifle", 6],
+            CALIBER_22WNR: ["-22-wmr", "rifle", 50],
+            CALIBER_300ACC: ["300-aac-blackout", "rifle", 55],
+            CALIBER_300WM: ["300winmag", "rifle", 56],
+            CALIBER_7REMMAG: ["7mm-rem-mag", "rifle", 57],
+            CALIBER_545X39: ["5-45x39", "rifle", 53],
             "243_Win": ["-243-win", "rifle", 52],
-            "5.45x39": ["5-45x39", "rifle", 53],
             "9.3x62": ["9-3x62", "rifle", 54],
-            "300_AAC": ["300-aac-blackout", "rifle", 55],
-            "300_Win_Mag": ["300winmag", "rifle", 56],
-            "7mm_Rem_Mag": ["7mm-rem-mag", "rifle", 57],
 
             "12_buckshot": ["12-67/12-70", "bore_buckshot", 60],
             "12_birdshot": ["12-65/12-67/12-70/12-76", "bore_birdshot", 61],
             "12_bulet": ["12-65/12-67/12-70/12-76", "bore_bulet", 62],
 
-            "9RA": ["9-mm", "traumatic", 70]
+            CALIBER_9RA: ["9-mm", "traumatic", 70]
         }
     },
     "shopgun": {
@@ -176,19 +177,19 @@ shops = {
             "traumatic": "travmaticheskie-patroni"
         },
         "category": {
-            "223_Rem": ["223-rem", "rifle", 0],
-            "308_Win": ["308-win", "rifle", 1],
-            "7.62x39": ["762-na-39", "rifle", 3],
-            "22_LR": ["22-lr", "rifle", 4],
-            "30-06": ["patrhone-30-06", "rifle", 5],
+            CALIBER_223REM: ["223-rem", "rifle", 0],
+            CALIBER_308WIN: ["308-win", "rifle", 1],
+            CALIBER_762X39: ["762-na-39", "rifle", 3],
+            CALIBER_22LR: ["22-lr", "rifle", 4],
+            CALIBER_3006: ["patrhone-30-06", "rifle", 5],
+            CALIBER_8X57JS: ["kalibr_16-70", "rifle", 7],
             "243_Win": ["243-win", "rifle", 6],
-            "8x57_JS": ["kalibr_16-70", "rifle", 7],
 
-            "12/70": ["patroni-12-calibra", "bore", 20],
-            "16/70": ["patroni-16-calibra", "bore", 21],
-            "20/70": ["patroni-18-calibra", "bore", 22],
+            CALIBER_1270: ["patroni-12-calibra", "bore", 20],
+            CALIBER_1670: ["patroni-16-calibra", "bore", 21],
+            CALIBER_2070: ["patroni-18-calibra", "bore", 22],
 
-            "9RA": ["kalibr_9-mm-r-a-", "traumatic", 30]
+            CALIBER_9RA: ["kalibr_9-mm-r-a-", "traumatic", 30]
         }
     },
     "tactical-systems": {
@@ -202,23 +203,23 @@ shops = {
             "bore": "gladkostvolnye-boepripasy"
         },
         "category": {
-            "223_Rem": ["22", "rifle", 0],
-            "308_Win": ["27", "rifle", 1],
-            "22_LR": ["24", "rifle", 2],
-            "7.62x39": ["13", "rifle", 3],
-            "5.45x39": ["32", "rifle", 4],
-            "9x21": ["26", "rifle", 5],
+            CALIBER_223REM: ["22", "rifle", 0],
+            CALIBER_308WIN: ["27", "rifle", 1],
+            CALIBER_22LR: ["24", "rifle", 2],
+            CALIBER_338LM: ["16", "rifle", 8],
+            CALIBER_762X39: ["13", "rifle", 3],
+            CALIBER_921: ["26", "rifle", 5],
+            CALIBER_300ACC: ["28,29s", "rifle", 10],
+            CALIBER_545X39: ["32", "rifle", 4],
             "9x19": ["4", "rifle", 6],
             "9x18": ["33", "rifle", 7],
-            "338_Lapua_Mag": ["16", "rifle", 8],
             "375_Chey_Tac": ["19", "rifle", 9],
-            "300_AAC": ["28,29s", "rifle", 10],
 
-            "12/70": ["23,25", "bore", 20],
-            "16/70": ["31", "bore", 21],
+            CALIBER_1270: ["23,25", "bore", 20],
+            CALIBER_1670: ["31", "bore", 21],
 
             "12_GA_traumatic": ["23", "traumatic", 30],
-            "9RA": ["34", "traumatic", 31],
+            CALIBER_9RA: ["34", "traumatic", 31],
         }
     },
     "tactica": {
@@ -238,20 +239,20 @@ shops = {
             "bore": 68,
         },
         "category": {
-            "223_Rem": ["[33:.223%20Rem%20(5.56%D1%8545)]", "rifle", 0],
-            "308_Win": ["[33:.308%20Win%20(7.62%D1%8551)]", "rifle", 1],
-            "22_LR": ["[33:.22LR]", "rifle", 2],
-            "7.62x39": ["[33:7.62x39]", "rifle", 3],
-            "9x21": ["[33:9x21]", "rifle", 5],
-            "30-06": ["[33:.30-06%20Sprg%20(7.62%D1%8563)]", "rifle", 6],
+            CALIBER_223REM: ["[33:.223%20Rem%20(5.56%D1%8545)]", "rifle", 0],
+            CALIBER_308WIN: ["[33:.308%20Win%20(7.62%D1%8551)]", "rifle", 1],
+            CALIBER_22LR: ["[33:.22LR]", "rifle", 2],
+            CALIBER_762X39: ["[33:7.62x39]", "rifle", 3],
+            CALIBER_921: ["[33:9x21]", "rifle", 5],
+            CALIBER_3006: ["[33:.30-06%20Sprg%20(7.62%D1%8563)]", "rifle", 6],
 
-            "12/70": ["[33:12/70]", "bore", 20],
-            "16/70": ["[33:16/70]", "bore", 21],
-            "20/70": ["[33:20/70]", "bore", 22],
+            CALIBER_1270: ["[33:12/70]", "bore", 20],
+            CALIBER_1670: ["[33:16/70]", "bore", 21],
+            CALIBER_2070: ["[33:20/70]", "bore", 22],
 
-            # "5.45x39": ["[33:5.45x39]", "rifle", 4],
-            # "300_Win_Mag": ["[33:.300%20Win%20Mag]", "rifle", 7],
-            # "300_AAC": ["[33:.300%20Whisper/Blackout%20(7.62%D1%8535)]", "rifle", 8],
+            # CALIBER_545X39: ["[33:5.45x39]", "rifle", 4],
+            # CALIBER_300WM: ["[33:.300%20Win%20Mag]", "rifle", 7],
+            # CALIBER_300ACC: ["[33:.300%20Whisper/Blackout%20(7.62%D1%8535)]", "rifle", 8],
             # "9x19": ["[33:9x19]", "rifle", 9],
             # "9x18": ["[33:9x18]", "rifle", 10],
         }
@@ -268,16 +269,16 @@ shops = {
             "bore_20": "hladkostvolni-patrony/20-kalibr",
         },
         "category": {
-            "22_LR": [349, "rifle", 0],
-            "308_Win": [105, "rifle", 1],
-            "30-06": [273, "rifle", 2],
-            "300_Win_Mag": [274, "rifle", 3],
-            "8x57": [278, "rifle", 4],
-            "22_WMR": [280, "rifle", 5],
+            CALIBER_22LR: [349, "rifle", 0],
+            CALIBER_308WIN: [105, "rifle", 1],
+            CALIBER_3006: [273, "rifle", 2],
+            CALIBER_300WM: [274, "rifle", 3],
+            CALIBER_8X57JS: [278, "rifle", 4],
+            CALIBER_22WNR: [280, "rifle", 5],
 
-            "12/70": [0, "bore_12", 10],
-            "16/70": [0, "bore_16", 11],
-            "20/70": [0, "bore_20", 12],
+            CALIBER_1270: [0, "bore_12", 10],
+            CALIBER_1670: [0, "bore_16", 11],
+            CALIBER_2070: [0, "bore_20", 12],
         }
     }
 }
